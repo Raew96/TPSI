73d1f435a5f2d0a0c5aba843fdbbaec05f125143
unknown <rafal.biarda1996@gmail.com>
2020-04-16 18:32:08 +0200

    lab 2 the last one

--- lab4v5/src/tpsi/web/ServletLab5.java
+++ lab4v5/src/tpsi/web/ServletLab5.java
@@ -37,16 +37,28 @@ public class ServletLab5 extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        int counter=1;
         HttpSession session = request.getSession();
         String firstName = request.getParameter("firstName");
         String lastName = request.getParameter("lastName");
         String emailAddress = request.getParameter("emailAddress");
         String groupId = request.getParameter("groupId");
 
-        ArrayList<Student> elo = (ArrayList<Student>)session.getAttribute("l");
-        elo.add(new Student(firstName, lastName, emailAddress, groupId));
-        session.setAttribute("l", elo);
-
+        if(session.getAttribute("counter") == null)
+        {
+            ArrayList<Student> l = new ArrayList<Student>();
+            session.setAttribute("counter", counter);
+            session.setAttribute("l", l);
+        }
+        else
+        {
+            counter = (int) session.getAttribute("counter");
+            counter++;
+            session.setAttribute("counter", counter);
+            ArrayList<Student> elo = (ArrayList<Student>)session.getAttribute("l");
+            elo.add(new Student(firstName, lastName, emailAddress, groupId));
+            session.setAttribute("l", elo);
+        }
         request.getRequestDispatcher("lab5.jsp").forward(request, response);
     }
 }
